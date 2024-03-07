fe566d5a499f2e2ba3f6e528cf9c73e455d8b9be
sunnyiuww <126841766+sunnyiuww@users.noreply.github.com>
2024-03-07 22:25:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// scrool reveral
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .educations-container, .certificates-box, .contact form', {origin: 'buttom'});
+ScrollReveal().reveal('.home-contact h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right'});
+
+// typed js
+var typed = new Typed('.multiple-text', {
+    strings: ['Information System Student'],
+    typeSpeed: 70,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|',
+    startDelay: 1000,
+    fadeOut: true,
+});
